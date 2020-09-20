710f10bf22c059c62e8e81550e7093c69c37fb02
Andres-Carranza <carranzamoreno9@gmail.com>
2020-09-20 14:31:23 -0500

    update

--- data_formatter.py
+++ data_formatter.py
@@ -4,36 +4,77 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from scipy.io import wavfile 
 from tqdm import tqdm
+from shutil import copyfile
 
 def check_dir(save_dir):
     if os.path.exists(save_dir) is False:
         os.makedirs(save_dir)
 
+def find_available(source,typ):
+    global acordeon_path
 
-acordeon_path = "/Users/andres/Documents/acordeon"
+    i = 0
+    check_name = '{}{}.{}'.format(source, i, typ)
 
-clips = os.listdir(os.path.join(acordeon_path,'raw','mp4'))
+    while os.path.exists(os.path.join(acordeon_path,source,check_name)):
+        i+=1
+        check_name ='{}{}.{}'.format(source, i, typ)
+    return check_name
 
-for clip in clips:
-    name = clip.split('.')[0]
-    clip_path = os.path.join(acordeon_path,'raw','mp3',name+'.mp3')
-    wav_path = os.path.join(acordeon_path,'raw','wav',name+'.wav')
-    if os.path.exists(wav_path) is False:
-        os.system('ffmpeg -i {} -vn -acodec pcm_s16le -ac 1 -ar 44100 -f wav {}'\
-            .format(clip_path, wav_path))
+def clean_data(sources, typ='mp4'):
+    global acordeon_path
+
+    for source in sources:
+        src_path = os.path.join(acordeon_path,source)
+        dest_path= os.path.join(acordeon_path, 'raw', typ)
+
+        check_dir(dest_path)
+
+        for i, fn in enumerate(sorted(os.listdir(src_path))):
+            if fn == '.DS_Store':
+                continue
+            nfn = '{}{}.{}'.format(source, i, typ)
+            #print(fn, nfn , end=' ')
+            if fn == nfn:
+                continue
+            if os.path.exists(os.path.join(acordeon_path, source, nfn)):
+                nfn = find_available(source,typ)
+                #print(nfn , end=' ')
+            os.rename(os.path.join(src_path,fn), os.path.join(src_path,nfn))
+        
+            copyfile(os.path.join(src_path,nfn), os.path.join(dest_path,nfn))
+    
+            #print()
+
+def to_wav( file_type='mp4'):
+    global acordeon_path
+
+    check_dir(os.path.join(acordeon_path,'raw/wav'))
+    clips = sorted(os.listdir(os.path.join(acordeon_path,'raw',file_type)))
+    
+    for clip in clips:
+        if clip == '.DS_Store':
+            continue
+        name = clip.split('.')[0]
+        clip_path = os.path.join(acordeon_path,'raw',file_type,'{}.{}'.format(name,file_type))
+        wav_path = os.path.join(acordeon_path,'raw','wav',name+'.wav')
+        if os.path.exists(wav_path) is False:
+            os.system('ffmpeg -i {} -vn -acodec pcm_s16le -ac 1 -ar 44100 -f wav {}'\
+                .format(clip_path, wav_path))
+
+def split_data(source,dest='raw/split',time =5):
+    global acordeon_path
+
+    clips = sorted(os.listdir(os.path.join(acordeon_path,source)))
+
+    if os.path.exists(os.path.join(acordeon_path,source,'.DS_Store')):
+        clips.remove('.DS_Store')
 
 
-acordeons = sorted(os.listdir(os.path.join(acordeon_path,'raw','wav')))
-
-train_path = os.path.join(acordeon_path, 'data', 'train','acordeon')
-valid_path = os.path.join(acordeon_path, 'data', 'valid','acordeon')
-
-train_fns, valid_fns = train_test_split(acordeons, test_size = 0.2)
-
-for split, fns in zip(['train', 'valid'], [train_fns, valid_fns]):
-    interval = 44100 * 5
-    for fn in tqdm(fns):
-        wav_path = os.path.join(acordeon_path,'raw','wav',fn)
+    for  fn in clips:  
+        interval = 44100 * time
+            
+        wav_path = os.path.join(acordeon_path,source,fn)
         rate, wav = wavfile.read(wav_path)
         
         stop = (wav.shape[0] // interval) * interval
@@ -41,14 +82,14 @@ for split, fns in zip(['train', 'valid'], [train_fns, valid_fns]):
         for i in range(0, stop-interval, interval):
             sample = wav[i:i+interval]
 
-            save_dir = os.path.join(acordeon_path, 'data', split, 'acordeon')
+            save_dir = os.path.join(acordeon_path, dest)
             check_dir(save_dir)
 
             fn = fn.split('.')[0]
 
             i = int(i/interval)
 
-            save_fn = str(os.path.join(save_dir, fn+'_{}.wav'.format(i)))
+            save_fn = str(os.path.join(save_dir,'{}_{}.wav'.format(fn,i)))
 
             if os.path.exists(save_fn):
                 continue
@@ -57,28 +98,92 @@ for split, fns in zip(['train', 'valid'], [train_fns, valid_fns]):
             data = sample)
 
 
-df = pd.read_csv(os.path.join(acordeon_path, 'ESC-50', 'meta', 'esc50.csv'))
 
-inter_dirs = np.unique(df.category).tolist()
 
-for cat in inter_dirs:
-    df_cat = df[df.category==cat]
+def format_acordeon_data():
+    global acordeon_path
 
-    train_df, valid_df = train_test_split(df_cat, test_size = 0.1)
+    acordeons = sorted(os.listdir(os.path.join(acordeon_path,'raw/split')))
 
-    for split, split_df in zip(['train', 'valid'], [train_df, valid_df]):
-        dir_path = os.path.join(acordeon_path, 'data', split,'interferer')
+    if os.path.exists(os.path.join(acordeon_path,'raw/split/.DS_Store')):
+        acordeons.remove('.DS_Store')
 
-        check_dir(dir_path)
 
-        for i, row in split_df.iterrows():
-            fn = row['filename']
-            fn_path = os.path.join(dir_path, fn)
+    train_fns, valid_fns = train_test_split(acordeons, test_size = 0.1,random_state=0)
 
-            if os.path.exists(fn_path):
+    for split, fns in zip(['train', 'valid'], [train_fns, valid_fns]):
+        for fn in tqdm(fns):
+            wav_path = os.path.join(acordeon_path,'raw/split',fn)
+
+            save_dir = os.path.join(acordeon_path, 'data', split, 'acordeon')
+            check_dir(save_dir)
+
+            save_fn = str(os.path.join(save_dir, fn))
+
+            if os.path.exists(save_fn):
                 continue
-            src_path = os.path.join(acordeon_path, 'ESC-50', 'audio', fn)
-            os.system('cp {} {}'.format(src_path, fn_path))
+
+            copyfile(wav_path,save_fn)
+
+def format_interferer_data():
+    global acordeon_path
+
+    clips = sorted(os.listdir(os.path.join(acordeon_path,'salsa/split')))
+
+    if os.path.exists(os.path.join(acordeon_path,'salsa/split/.DS_Store')):
+        clips.remove('.DS_Store')
 
 
+    train_fns, valid_fns = train_test_split(clips, test_size = 0.1,random_state=0)
 
+    for split, fns in zip(['train', 'valid'], [train_fns, valid_fns]):
+        for fn in tqdm(fns):
+            wav_path = os.path.join(acordeon_path,'salsa/split',fn)
+
+            save_dir = os.path.join(acordeon_path, 'data', split, 'interferer')
+            check_dir(save_dir)
+
+            save_fn = str(os.path.join(save_dir, fn))
+
+            if os.path.exists(save_fn):
+                continue
+
+            copyfile(wav_path,save_fn)
+
+
+acordeon_path = "/Users/andres/Documents/acordeon"
+
+sources = ['franfaleromusic', 'juanjosegranados','silveravallenato']
+#clean_data(sources)
+#to_wav('mp4')
+#split_data('raw/wav')
+#format_acordeon_data()
+#split_data('salsa/raw','salsa/split')
+
+format_interferer_data()
+
+'''def format_interferer_data():
+    global acordeon_path
+
+    df = pd.read_csv(os.path.join(acordeon_path, 'salsa', 'split', 'esc50.csv'))
+
+    inter_dirs = np.unique(df.category).tolist()
+
+    for cat in inter_dirs:
+        df_cat = df[df.category==cat]
+
+        train_df, valid_df = train_test_split(df_cat, test_size = 0.1)
+
+        for split, split_df in zip(['train', 'valid'], [train_df, valid_df]):
+            dir_path = os.path.join(acordeon_path, 'data', split,'interferer')
+
+            check_dir(dir_path)
+
+            for i, row in split_df.iterrows():
+                fn = row['filename']
+                fn_path = os.path.join(dir_path, fn)
+
+                if os.path.exists(fn_path):
+                    continue
+                src_path = os.path.join(acordeon_path, 'ESC-50', 'audio', fn)
+                os.system('cp {} {}'.format(src_path, fn_path))'''
